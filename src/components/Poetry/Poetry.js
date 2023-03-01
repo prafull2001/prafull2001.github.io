b3eb8a6d0af01aa7c8033ec67defc0a628ad84ab
Prafull Sharma <prafull2001@gmail.com>
2023-02-28 23:13:40 -0600

    created basic poetry publishing format

--- src/components/Poetry/Poetry.js
+++ src/components/Poetry/Poetry.js
@@ -1,11 +1,49 @@
-import React, { useState, useEffect } from "react";
+import React from 'react';
 import { Container, Row } from "react-bootstrap";
+import { Accordion, Card, Button } from 'react-bootstrap';
+
 
 
 function Poetry() {
   return (
     <Container fluid className="about-section">
-      <h1> Poetry </h1>
+        <h1 style={{ fontSize: "2.6em", textAlign: "left" }}>
+            First <span className="purple"> Poem </span>
+            </h1>
+        <Accordion defaultActiveKey="0">
+            <Accordion.Item eventKey="0">
+                <Accordion.Header>Accordion Item #1</Accordion.Header>
+                    <Accordion.Body>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                        culpa qui officia deserunt mollit anim id est laborum.
+                    </Accordion.Body>
+            </Accordion.Item>
+        </Accordion>
+
+        <h1 style={{ fontSize: "2.6em", textAlign: "left" }}>
+            Second <span className="purple"> Poem </span>
+            </h1>
+        <Accordion defaultActiveKey="0">
+            <Accordion.Item eventKey="0">
+                <Accordion.Header>Accordion Item #1</Accordion.Header>
+                    <Accordion.Body>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+                        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                        aliquip ex ea commodo consequat. Duis aute irure dolor in
+                        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+                        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                        culpa qui officia deserunt mollit anim id est laborum.
+                    </Accordion.Body>
+            </Accordion.Item>
+        </Accordion>
+
+        
     </Container>
   );
 }
